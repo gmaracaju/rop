d10fdefe328012c8b19317ee347299a23619df0c
gmaracaju <estatistica.gma@gmail.com>
2026-05-15 20:28:10 -0300

    Replace 'Polícia Municipal' with 'Guarda Municipal'

--- script.js
+++ script.js
@@ -615,7 +615,7 @@ function adicionarAgente() {
                 <option>Subinspetor de 2ª Classe</option>
                 <option>Subinspetor de 3ª Classe</option>
                 <option>Supervisor</option>
-                <option>Polícia Municipal</option>
+                <option>Guarda Municipal</option>
             </select>
             <span class="select-print" style="display:none;"></span>
         </td>
